d7f3ab4a4b3b448085bc7c25e135a5d1b296f7c5
shushengyuan <2637975471@qq.com>
2023-03-24 21:07:22 +0800

    feat: 添加redis

--- controller/init_redis.go
+++ controller/init_redis.go
@@ -1,5 +1,20 @@
 package controller
 
-import "github.com/go-redis/redis"
+import (
+	"context"
+
+	"github.com/go-redis/redis"
+)
 
 var RdbLikeVideoId *redis.Client //key:VideoId,value:userId
+var Ctx = context.Background()
+
+var RdbLikeUserId *redis.Client //key:userId,value:VideoId
+
+func InitRedis() {
+	RdbLikeUserId = redis.NewClient(&redis.Options{
+		Addr:     "106.14.75.229:6379",
+		Password: "tiktok",
+		DB:       5, //  选择将点赞视频id信息存入 DB5.
+	})
+}
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	r := gin.Default()
 
 	initRouter(r)
+	controller.InitRedis()
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
